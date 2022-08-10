3a497dce24013df1e48e623bf27905705c3a3909
Alejae1998 <alejagr1998@gmail.com>
2022-08-10 11:52:15 -0700

    adding first test and failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,19 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+const { agent } = require('supertest');
+jest.mock('../lib/services/github');
 
-describe('backend-express-template routes', () => {
+describe('oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET redirect to the github oauth page upon login', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
   });
   afterAll(() => {
     pool.end();
