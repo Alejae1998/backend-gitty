4243630f78319ad02a5476f5d64617185dd2ded9
Alejae1998 <alejagr1998@gmail.com>
2022-08-10 12:40:51 -0700

    adding second GET test and failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,6 +15,21 @@ describe('oauth routes', () => {
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
   });
+  it('GET callback URI for Github to redirect to after login', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
   afterAll(() => {
     pool.end();
   });
